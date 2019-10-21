5384dfe6e7240ffef34d30be3ca8ecfa111dd8ac
andres-martin <andresmartin266@gmail.com>
2019-10-21 15:57:36 -0500

    [add] annotate upload model

--- api_jwt_v3/app/models/upload.rb
+++ api_jwt_v3/app/models/upload.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: uploads
+#
+#  id         :integer          not null, primary key
+#  url        :string
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Upload < ApplicationRecord
 end
--- api_jwt_v3/test/models/upload_test.rb
+++ api_jwt_v3/test/models/upload_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: uploads
+#
+#  id         :integer          not null, primary key
+#  url        :string
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class UploadTest < ActiveSupport::TestCase
