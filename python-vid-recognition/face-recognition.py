59094ef27bca2b451a47163b3493f94db5c47e43
klupzor <lopezgand@gmail.com>
2019-10-22 13:44:17 -0500

    Updating code

--- python-vid-recognition/face-recognition.py
+++ python-vid-recognition/face-recognition.py
@@ -15,7 +15,9 @@ known_faces = [
 # Initialize variables
 face_locations = []
 face_encodings = []
-face_names = []
+face_names = [
+    "andres"
+]
 frame_number = 0
 
 while True:
@@ -27,23 +29,23 @@ while True:
     if not ret:
         break
 
-    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    # Convert the image from BGR color (which OpenCV uses) to RGB color
+    # (which face_recognition uses)
     rgb_frame = frame[:, :, ::-1]
 
     # Find all the faces and face encodings in the current frame of video
     face_locations = face_recognition.face_locations(rgb_frame, model="cnn")
     face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
 
-    face_names = []
     for face_encoding in face_encodings:
         # See if the face is a match for the known face(s)
-        match = face_recognition.compare_faces(known_faces, face_encoding, tolerance=0.50)
+        match = face_recognition.compare_faces(
+            known_faces, face_encoding)
 
-        name = None
-        if match[0]:
-            name = "Andres"
-
-        face_names.append(name)
+        name = "Unknow"
+        if True in match:
+            first_match_index = match.index(True)
+            name = face_names[first_match_index]
 
     # Label the results
     for (top, right, bottom, left), name in zip(face_locations, face_names):
@@ -54,12 +56,13 @@ while True:
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
         # Draw a label with a name below the face
-        cv2.rectangle(frame, (left, bottom - 25), (right, bottom), (0, 0, 255), cv2.FILLED)
+        cv2.rectangle(frame, (left, bottom - 25),
+                      (right, bottom), (0, 0, 255), cv2.FILLED)
         font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6), font, 0.5, (255, 255, 255), 1)
+        cv2.putText(frame, name, (left + 6, bottom - 6),
+                    font, 0.5, (255, 255, 255), 1)
 
     # Write the resulting image to the output video file
-       # Display the resulting image
     cv2.imshow('Video', frame)
 
     # Hit 'q' on the keyboard to quit!
