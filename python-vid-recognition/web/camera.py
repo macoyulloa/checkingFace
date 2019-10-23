10e816d1c63f56bfe2aad582a26ebaf8aeebfed4
klupzor <lopezgand@gmail.com>
2019-10-23 11:32:28 -0500

    Conecting face recognition with html

--- python-vid-recognition/web/camera.py
+++ python-vid-recognition/web/camera.py
@@ -1,37 +1,98 @@
+import face_recognition
 import cv2
-
-
-WHITE = [255, 255, 255]
-face_cascade = cv2.CascadeClassifier('Haar/haarcascade_frontalcatface.xml')
-eye_cascade = cv2.CascadeClassifier('Haar/haarcascade_eye.xml')
-
-
-def draw_box(Image, x, y, w, h):
-    cv2.line(Image, (x, y), (x + int(w / 5), y), WHITE, 2)
-    cv2.line(Image, (x + int((w / 5) * 4), y), (x + w, y), WHITE, 2)
-    cv2.line(Image, (x, y), (x, y + int(h / 5)), WHITE, 2)
-    cv2.line(Image, (x + w, y), (x + w, y + int(h / 5)), WHITE, 2)
-    cv2.line(Image, (x, (y + int(h / 5 * 4))), (x, y + h), WHITE, 2)
-    cv2.line(Image, (x, (y + h)), (x + int(w / 5), y + h), WHITE, 2)
-    cv2.line(Image, (x + int((w / 5) * 4), y + h), (x + w, y + h), WHITE, 2)
-    cv2.line(Image, (x + w, (y + int(h / 5 * 4))), (x + w, y + h), WHITE, 2)
+import numpy as np
+import io
+import socket
+import struct
+import time
+import pickle
+import zlib
 
 
 class VideoCamera(object):
     def __init__(self):
-        self.video = cv2.VideoCapture(0)
+        self.video_capture = cv2.VideoCapture(0)
 
     def __del__(self):
         self.video.release()
 
     def get_frame(self):
-        success, image = self.video.read()
-        # We are using Motion JPEG, but OpenCV defaults to capture raw images,
-        # so we must encode it into JPEG in order to correctly display the
-        # video stream.
+        # Load a sample picture and learn how to recognize it.
+        image = face_recognition.load_image_file("./data-images/andres.png")
+        face_encoding = face_recognition.face_encodings(image)[0]
 
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # Create arrays of known face encodings and their names
+        known_face_encodings = [
+            face_encoding
+        ]
+        known_face_names = [
+            "Andres"
+        ]
 
-        ret, jpeg = cv2.imencode('.jpg', gray)
+        # Initialize some variables
+        face_locations = []
+        face_encodings = []
+        face_names = []
+        process_this_frame = True
 
-        return jpeg.tobytes()
+        # Grab a single frame of video
+        ret, frame = self.video_capture.read()
+
+        # Resize frame of video to 1/4 size for faster face recognition
+        # processing
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+        # face_recognition uses)
+        rgb_small_frame = small_frame[:, :, ::-1]
+
+        # Only process every other frame of video to save time
+        if process_this_frame:
+            # Find all the faces and face encodings in the current frame of
+            # video
+            face_locations = face_recognition.face_locations(rgb_small_frame)
+            face_encodings = face_recognition.face_encodings(
+                rgb_small_frame, face_locations)
+
+            face_names = []
+            for face_encoding in face_encodings:
+                # See if the face is a match for the known face(s)
+                matches = face_recognition.compare_faces(
+                    known_face_encodings, face_encoding, tolerance=0.5)
+                name = "Unknown"
+
+                # new face
+                face_distances = face_recognition.face_distance(
+                    known_face_encodings, face_encoding)
+                best_match_index = np.argmin(face_distances)
+                if matches[best_match_index]:
+                    name = known_face_names[best_match_index]
+
+                face_names.append(name)
+
+        process_this_frame = not process_this_frame
+
+        # Display the results
+        for (
+                top, right, bottom, left), name in zip(
+                face_locations, face_names):
+            # Scale back up face locations since the frame we detected in was
+            # scaled to 1/4 size
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+
+            # Draw a box around the face
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+            # Draw a label with a name below the face
+            cv2.rectangle(frame, (left, bottom - 35),
+                          (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6),
+                        font, 1.0, (255, 255, 255), 1)
+        encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+        # Display the resulting image
+        result, jpg = cv2.imencode('.jpg', frame, encode_param)
+
+        return jpg.tobytes()
