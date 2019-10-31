13ac43cff9ad7708e4e172f3215635e1188cfbbe
klupzor <lopezgand@gmail.com>
2019-10-31 18:36:24 -0500

    sending post method

---
+++ landing-page/src/javaScript.js
@@ -0,0 +1,16 @@
+const myForm = document.getElementById('myForm');
+
+function sendData(){
+    var dataArray = $("form").serializeArray();
+    var data = {}
+
+    dataArray.map(function fun (obj) {
+        data[obj.name] = obj.value;
+    })
+
+    $.ajax({    
+        headers: {'Content-Type':'application/json'}, method: "POST",
+        url: "https://api-jwt-v3.herokuapp.com/api/users",
+        data: JSON.stringify({ user: data})
+       })
+}
